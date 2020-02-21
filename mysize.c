73b6c4cdec322196366d7c3893cd8babe53fcb88
Pablo T Campos <55254526+burned301@users.noreply.github.com>
2020-02-21 14:26:43 +0100

    mysize.c finished
    Signed-off-by: Paburo <pablotcampos25@gmail.com>

--- mysize.c
+++ mysize.c
@@ -1,4 +1,51 @@
-//
-// Created by jorge on 20/2/20.
-//
+#include <fcntl.h>
+//OPENDIR
+#include <dirent.h>
+//READ
+#include <unistd.h>
+//Basicas C
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+int main(int argc, char **argv){
+    //Comprueba que como máximo se pasa un único parámetro
+    if(argc !=1 ){
+        write(STDERR_FILENO,"Numero de argumentos invalido\n",29);
+        exit(-1);
+    }
+
+    //Almacenamos en path la ruta del directorio en el que se ejecuta mysize.c
+    //Abrimos el directorio en cuestión, almacenándolo en dir
+    DIR* dir;
+    char path[PATH_MAX];
+    getcwd(path, PATH_MAX);
+    dir = opendir(path);
+
+    //Comprobamos si el directorio existe
+    if(dir==NULL){
+        write(STDERR_FILENO,"No se ha podido abrir el directorio\n",36);
+        exit(-1);
+    }
+
+    //Leemos los ficheros del directorio
+    struct dirent* rdir;
+    rdir = readdir(dir);
+    while(rdir!=NULL){
+        if(rdir->d_type==DT_REG){
+            int fr = open(rdir->d_name, O_RDONLY);
+            int size;
+            //Abrimos todos los ficheros y almacenamos su tamaño en size
+            if(fr != -1){
+                size =  lseek(fr,0,SEEK_END);
+                close(fr);
+                printf("%s\t%d\n",rdir->d_name,size);
+            }
+        }
+        rdir = readdir(dir);
+    }
+
+    //Cerramos el directorio y retornamos 0
+    closedir(dir);
+    return 0;
+}
