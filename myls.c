f720384b73ac23578d7c09bb9066380ecd1e2ec5
Pablo T Campos <55254526+burned301@users.noreply.github.com>
2020-02-21 11:11:59 +0100

    mysl.c created (pending local route and testing)

--- myls.c
+++ myls.c
@@ -1,4 +1,48 @@
-//
-// Created by jorge on 20/2/20.
-//
+//OPENDIR
+#include <sys/types.h>
+#include <dirent.h>
 
+#include <fcntl.h>
+//READ
+#include <unistd.h>
+//Basicas C
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char **argv){
+
+    if(argc>2){
+        perror("Numero de argumentos invalido");
+        exit(-1);
+    }
+
+    DIR* dir;
+    char *path;
+    if(argc==1){
+        //RELATIVA
+
+
+    }else{
+        if(sizeof(argv[1])>PATH_MAX){
+            perror("Ruta demasiado grande");
+            exit(-1);
+        }
+        path = argv[1];
+    }
+    dir = opendir(path);
+    if(dir==NULL){
+        perror("No se ha podido abrir el directorio");
+        exit(-1);
+    }
+    struct dirent* rdir;
+    rdir = readdir(dir);
+    while(rdir!=NULL){
+        printf("%s\n",rdir->d_name);
+        rdir = readdir(dir);
+    }
+    closedir(dir);
+
+    return 0;
+
+}
