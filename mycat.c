acb2372684041ee6271119b22a507d2b1f531bbf
Pablo T Campos <55254526+burned301@users.noreply.github.com>
2020-02-21 17:21:06 +0100

    mycat.c ahora es legible
    Signed-off-by: Paburo <pablotcampos25@gmail.com>

--- mycat.c
+++ mycat.c
@@ -13,25 +13,31 @@ int main(int argc, char **argv){
         write(STDERR_FILENO,"Numero de argumentos invalido\n",31);
         exit(-1);
     }
+
     //Si son validos abrimos el fichero que ha sido indicado en el primer argumento como solo lectura
     int fr = open(argv[1],O_RDONLY);
+
     //Comprobamos que haya podido abrir el fichero
     if( fr== -1){
         //Si no ha podido imprimimos por pantalla el error y terminamos la ejecucion
         perror("No se ha podido abrir el fichero");
         exit(-1);
     }
+
     //Creamos un buffer del tamaño especificado y un entero que almacenara el numero de bytesleido
     char buffer [1024];
     int bytesleidos;
+
     //Leemos tantos bytes como el tamaño del buffer
     bytesleidos= read(fr, &buffer, sizeof(buffer));
+
     //Iteramos hasta que no leamos ningun byte en una iteracion
     while (bytesleidos){
         //Escribimos por pantalla los bytes leidos y volvemos a leer
         write(STDOUT_FILENO,&buffer, bytesleidos);
         bytesleidos= read(fr, &buffer, sizeof(buffer));
     }
+
     //Cerramos el fichero y finalizamos la ejeccución exitosamente
     close(fr);
     return 0;
